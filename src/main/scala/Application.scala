6303f16b4b4920b2214b3557743bccc7f2540b70
Orest Ohorodnyk <o.ohorodnyk@gmail.com>
2021-01-09 17:51:19 +0200

    parsed message

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,23 +1,54 @@
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.stream.scaladsl.{Broadcast, BroadcastHub, Flow, GraphDSL, Keep, Merge, Sink, Source}
+import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
+import akka.http.scaladsl.model.sse.ServerSentEvent
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import akka.stream.ThrottleMode
+import akka.stream.alpakka.sse.scaladsl.EventSource
+import akka.stream.scaladsl.{Sink, Source}
+import spray.json.DefaultJsonProtocol
 
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContextExecutor, Future}
-import akka.http.scaladsl.model.sse.ServerSentEvent
-import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse, Uri}
-import akka.stream.ThrottleMode
-import akka.stream.alpakka.sse.scaladsl.EventSource
-import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
-case class Message(id: String, user: String, `type`: String)
+case class Meta(
+                 uri: String,
+                 request_id: String,
+                 id: String,
+                 dt: String,
+                 domain: String,
+                 stream: String,
+                 topic: String,
+                 partition: Int,
+                 offset: Long
+               )
+
+case class Message(
+                    `$schema`: String,
+                    meta: Meta,
+                    //                     id: Int, // some message missing this field and it cause flow failure
+                    `type`: String,
+                    namespace: Int,
+                    title: String,
+                    comment: String,
+                    timestamp: Int,
+                    user: String,
+                    bot: Boolean,
+                    server_url: String,
+                    server_name: String,
+                    server_script_path: String,
+                    wiki: String,
+                    parsedcomment: String
+
+                  )
 
 object MyMessageJsonProtocol extends DefaultJsonProtocol {
-  implicit val messageFormat = jsonFormat3(Message.apply)
+  implicit val metaFormat = jsonFormat9(Meta.apply)
+  implicit val messageFormat = jsonFormat14(Message.apply)
 
 }
+
 
 import MyMessageJsonProtocol._
 import spray.json._
@@ -42,9 +73,12 @@ object Application extends App {
   val events =
     eventSource
       .throttle(elements = 1, per = 500.milliseconds, maximumBurst = 1, ThrottleMode.Shaping)
-//      .take(10)
+      //      .take(10)
       .map(_.getData())
-      //      .map(_.parseJson.convertTo[Message])
-      .runWith(Sink.foreach(println))
-
+      .map(_.parseJson.convertTo[Message])
+      //      .runWith(Sink.onComplete {
+      //        case Success(message) => println(message)
+      //        case Failure(t) => println("An error has occurred: " + t.getMessage + t.fillInStackTrace() + t.getLocalizedMessage)
+      //      })
+      .runWith(Sink.foreach(message => println(message.title)))
 }
